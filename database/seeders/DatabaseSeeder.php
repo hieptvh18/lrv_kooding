6673f5c86effbfde0ac052c2c2e43f7cc8a6b558
hieptvh <hieptvh18@gmail.com>
2022-05-10 23:17:27 +0700

    update render data seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,71 +17,70 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         // seeder
 
         // roles user
-        // DB::table('roles')->insert([
-        //     [
-        //         "name"=>"Khách hàng",
-        //         "created_at"=> Carbon::now()->toDateTimeString(),
-        //         "updated_at"=> Carbon::now()->toDateTimeString()
-        //     ],
-        //     [
-        //         "name"=>"Nhân viên",
-        //         "created_at"=> Carbon::now()->toDateTimeString(),
-        //         "updated_at"=> Carbon::now()->toDateTimeString()
+        DB::table('roles')->insert([
+            [
+                "name"=>"Khách hàng",
+                "created_at"=> Carbon::now()->toDateTimeString(),
+                "updated_at"=> Carbon::now()->toDateTimeString()
+            ],
+            [
+                "name"=>"Nhân viên",
+                "created_at"=> Carbon::now()->toDateTimeString(),
+                "updated_at"=> Carbon::now()->toDateTimeString()
 
-        //     ],
-        //     [
-        //         "name"=>"Quản lý",
-        //         "created_at"=> Carbon::now()->toDateTimeString(),
-        //         "updated_at"=> Carbon::now()->toDateTimeString()
+            ],
+            [
+                "name"=>"Quản lý",
+                "created_at"=> Carbon::now()->toDateTimeString(),
+                "updated_at"=> Carbon::now()->toDateTimeString()
 
-        //     ]
-        // ]);
+            ]
+        ]);
 
-        // // account
-        // DB::table('users')->insert([
-        //     [
-        //         "name"=>"hiep hoang tran",
-        //     "email"=>"hieptvh18@gmail.com",
-        //     "phone"=>"0123144444",
-        //     "role_id"=>3,
-        //     "password"=>bcrypt("123123")
-        //     ],
-        //     [
-        //         "name"=>"toi la khach hang",
-        //     "email"=>"hipbu18@gmail.com",
-        //     "phone"=>"0123144442",
-        //     "role_id"=>1,
-        //     "password"=>bcrypt("123123")
-        //     ]
-        // ]);
+        // account
+        DB::table('users')->insert([
+            [
+                "name"=>"hiep hoang tran",
+            "email"=>"hieptvh18@gmail.com",
+            "phone"=>"0123144444",
+            "role_id"=>3,
+            "password"=>bcrypt("123123")
+            ],
+            [
+                "name"=>"toi la khach hang",
+            "email"=>"hipbu18@gmail.com",
+            "phone"=>"0123144442",
+            "role_id"=>1,
+            "password"=>bcrypt("123123")
+            ]
+        ]);
 
-        // // attribute
-        // DB::table('attributes')->insert([
-        //     [
-        //         "name"=>"Màu sắc"
-        //     ],
-        //     [
-        //         "name"=>"Kích cỡ"
-        //     ],
-        //     ["name"=>"Chất liệu"]
-        // ]);
+        // attribute
+        DB::table('attributes')->insert([
+            [
+                "name"=>"Màu sắc"
+            ],
+            [
+                "name"=>"Kích cỡ"
+            ],
+            ["name"=>"Chất liệu"]
+        ]);
 
-        // // attr value
-        // DB::table('attr_values')->insert([
-        //     ["attr_id"=>1,
-        //     "name"=>"Đỏ cam",
-        //     "value"=>"#ff0000"],
-        //     ["attr_id"=>2,
-        //     "name"=>"Medium",
-        //     "value"=>"M"],
-        //     ["attr_id"=>3,
-        //     "name"=>"Vải gấm",
-        //     "value"=>"Vải gấm"],
-        // ]);
+        // attr value
+        DB::table('attr_values')->insert([
+            ["attr_id"=>1,
+            "name"=>"Đỏ cam",
+            "value"=>"#ff0000"],
+            ["attr_id"=>2,
+            "name"=>"Medium",
+            "value"=>"M"],
+            ["attr_id"=>3,
+            "name"=>"Vải gấm",
+            "value"=>"Vải gấm"],
+        ]);
 
         // web_setting
         DB::table('web_settings')->insert([
